4e9e99fb04761d7a7b68721357495dd8fa7bd80e
Aya Refaat <aya180084@fci.bu.edu.eg>
2023-02-09 00:54:00 +0200

    Form & Home Page Components

---
+++ src/Components/Form/Admin/adminForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../../CSS/globalFile.css';
+import '../form.css';
+
+export default function AdminForm() {
+  return (
+    <React.StrictMode>
+     
+    </React.StrictMode>
+  )
+}
---
+++ src/Components/Form/User/userForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../../../CSS/globalFile.css';
+import '../form.css'
+
+export default function UserForm() {
+  return (
+    <React.StrictMode>
+      <div className="container">
+        
+      </div>
+    </React.StrictMode>
+  )
+}
