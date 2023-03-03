fe17f6a8ac818c60f5566ead074135f7883b5497
emanwael <emywael194@gmail.com>
2023-03-03 12:07:37 +0200

    add popup view details in menu

--- src/pages/restaurant-profile/components/common/Menu/MenuItem/index.jsx
+++ src/pages/restaurant-profile/components/common/Menu/MenuItem/index.jsx
@@ -4,6 +4,7 @@ import ButtonAddRemoveItem from "../../ButtonAddRemoveItem";
 import "./styles.css";
 import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../../../../../store/slices/cart";
+import Popup from "reactjs-popup";
 
 const MenuItem = ({ item }) => {
   const { _id, meal_name, meal_img, price, description } = item;
@@ -39,6 +40,18 @@ const MenuItem = ({ item }) => {
         <p className="head_desc-info">
           <small>{description}</small>
         </p>
+        <Popup
+          trigger={
+            <button className=" btn-detais fw-bold">
+              view detials &rarr;{" "}
+            </button>
+          }
+          modal
+          nested
+        >
+          {(close) => popup({ item, close })}
+        </Popup>
+        {/* <button className=" btn-detais">view detials &rarr; </button> */}
       </div>
       <div className="item-foot_desc">
         <span className="foot_desc-price">${price}</span>
@@ -53,3 +66,34 @@ const MenuItem = ({ item }) => {
 };
 
 export default MenuItem;
+
+const popup = ({ item, close }) => {
+  const { _id, meal_name, meal_img, price, description, food_group } = item;
+
+  return (
+    <div className=" text-center align-content-center p-3">
+      <div className="row">
+        <div className="col-6">
+          <img src={meal_img} alt="" className=" imgg" />
+        </div>
+        <div className="col-6 row text-start align-content-center">
+          <p className=" col-12  fs-2">{meal_name}</p>
+          <p className=" col-12">{description}</p>
+          <p className=" col-12">
+            <span className=" fw-bold text-black">food group :</span>{" "}
+            {food_group}
+          </p>
+          <p className=" col-12 fw-bold">${price}</p>
+        </div>
+      </div>
+      <button
+        className=" bgg text-white p-2 m-2"
+        onClick={() => {
+          close();
+        }}
+      >
+        Close
+      </button>
+    </div>
+  );
+};
